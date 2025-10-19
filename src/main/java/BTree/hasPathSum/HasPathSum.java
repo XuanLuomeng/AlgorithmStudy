8e24dbb6c0c3fc6a2df969b678475ab9dd9c1ba5
luojiawei <1079716693@qq.com>
2025-10-19 15:59:22 +0800

    HasPathSum

--- src/main/java/BTree/hasPathSum/HasPathSum.java
+++ src/main/java/BTree/hasPathSum/HasPathSum.java
@@ -3,8 +3,13 @@ package BTree.hasPathSum;
 /**
  * @author LuoXuanwei
  * @version 1.0
- * @description
+ * @description 路径总和
  * @date 2025/10/19 15:49
+ *
+ * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。如果存在，返回 true ；否则，返回 false 。
+ *
+ * 叶子节点 是指没有子节点的节点。
+ *
  * <p>
  * 思路:
  * 深度优先遍历
