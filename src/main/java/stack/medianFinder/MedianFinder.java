4c3cfe477296bfe1c9f9385610cf5f44a6095382
luojiawei <1079716693@qq.com>
2025-11-24 14:48:59 +0800

    栈:数据流的中位数

---
+++ src/main/java/stack/medianFinder/MedianFinder.java
@@ -0,0 +1,45 @@
+package stack.medianFinder;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author LuoXuanwei
+ * @version 1.0
+ * @description 数据流的中位数
+ * @date 2025/11/24 11:24
+ *
+ * 中位数是有序整数列表中的中间值。如果列表的大小是偶数，则没有中间值，中位数是两个中间值的平均值。
+ * 例如 arr = [2,3,4] 的中位数是 3 。
+ * 例如 arr = [2,3] 的中位数是 (2 + 3) / 2 = 2.5 。
+ * 实现 MedianFinder 类:
+ * MedianFinder() 初始化 MedianFinder 对象。
+ * void addNum(int num) 将数据流中的整数 num 添加到数据结构中。
+ * double findMedian() 返回到目前为止所有元素的中位数。与实际答案相差 10-5 以内的答案将被接受。
+ */
+public class MedianFinder {
+    private PriorityQueue<Integer> minHeap;
+    private PriorityQueue<Integer> maxHeap;
+
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>((a, b) -> a - b);
+        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+    }
+
+    public void addNum(int num) {
+        if (maxHeap.size() == minHeap.size()) {
+            minHeap.offer(num);
+            maxHeap.offer(minHeap.poll());
+        } else {
+            maxHeap.offer(num);
+            minHeap.offer(maxHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        } else {
+            return Double.valueOf(maxHeap.peek());
+        }
+    }
+}
